c0ad4deba8df473ec14df4343a8b63c39f80cf6a
myding <johndoe@example.com>
2022-12-14 13:56:28 +0100

    Small fixes

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,7 +4,10 @@
     {
         MessageService messageService = new MessageService();
 
+        // Till Uppgift 1.
         //messageService.OnMessageAdded = ;
+
+        messageService.AddMessage(new Message("Ang. hundfrisyren", "Jag önskar en helpermanentning av Fido, inte rakning. Hoppas mitt meddelande når dig i tid.", "Lasse på macken", "Sax&Koppel AB"));
 
         // 2. Avkommentera denna inför uppgift 2 i MessageService.
         //messageService.ProcessMessages(FilterBadWords);
@@ -12,6 +15,7 @@
 
     // #1B - Skriv en metod som OnMessageAdded kan referera till. Låt metoden skriva ut vem som skickat ett meddelande till vem.
 
+    // Till uppgift 2:
     public static int FilterBadWords(Message msg)
     {
         string badWord = "järnspikars";
@@ -19,5 +23,4 @@
         msg.Body = msg.Body.Replace(badWord, "****");
         return count;
     }
-
 }
