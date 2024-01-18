33c2fbd46fbb3d92e9899a72a1f122f5357d7374
Gus <gusgit@fastmail.org>
2024-01-18 15:02:45 +0100

    Update comments

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -2,23 +2,26 @@
 {
     private static void Main(string[] args)
     {
-        // 1. Två listor/arrayer med begränsat/litet innehåll
-        // Skapa två listor med enkelt innehåll, ex strängar eller ints
-        // LINQ: where, distinct, union, intersect / except
+        // 1. Skapa två listor med Users. Använd User.GetRandomListOfUsers(1000) för att skapa listorna.
+        // 2. Vad kan du använda Where(), Distinct(), Union(), Intersect() till? Vilken typ av 
 
 
         // 2. Två listor med mycket innehåll. Faker.Net
-        // Skapa först klass för User
-        // Skapa funktionalitet för att skapa listor med x antal user-objekt
-
-
+        //Använd metoden User.GetRandomListOfUsers(1000) för att skapa en lista med 1000 användare.
         //Hur kan du ta reda på vilket land som har användare med högst totala DataStored?
         //Alltså, räkna ihop den totala DataStored per land och skriv ut det land som ligger högst.
         //Använd dig av så mkt LINQ som möjligt, ex Sum() och Max().  (Hint: Select() behövs också...)
 
 
         // 3. Använda LINQ på ännu större datakällor
-        // Läsa in en textfil
-        // Förbereda textfilen för analys med LINQ
+        // Läs in textfilen pg46.txt
+        // Hur kan du använda LINQ för att analysera den? Exempelvis:
+        // - Hur många ord finns det i texten?
+        // - Hur många ord börjar på bokstaven "a"?
+        // - Vilket är det vanligaste ordet?
+        // - Hur många ord är längre än 10 tecken?
+        // - Hur många ord är unika? (Hint: Distinct() )
+        // - Hur många ord förekommer mer än 10 gånger? (Hint: GroupBy() )
+        // - Vilka ord förekommer endast en gång?
     }
 }
